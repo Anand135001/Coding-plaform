10bec2efb160b66b9329ca09cc23af7bd2a8bd72
Anand <aanandmainwal@gmail.com>
2025-07-15 10:29:58 +0530

    Initialize backend with Express and MongoDB connection

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const main = require('./config/db');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+app.use(express());
+app.use(cookieParser());
+
+main()
+.then(async ()=>{
+   app.listen(process.env.PORT, () => {
+      console.log("Running at port number:"+ process.env.PORT);
+      
+   })
+})
+.catch(err => console.log("Error Occurred:", +err));
