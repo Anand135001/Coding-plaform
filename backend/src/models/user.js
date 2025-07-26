b502eef5d27aabcc8755c96b71edf5a38481c055
Anand <aanandmainwal@gmail.com>
2025-07-26 15:10:12 +0530

    Add user model schema with validation and default values

---
+++ backend/src/models/user.js
@@ -0,0 +1,48 @@
+const mongoose =  require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+      
+    firstname: {
+        type: String,
+        require: true,
+        unique: true,
+        trim: true,
+        minlength: 2,
+        maxlength: 20,
+    },
+    Lastname: {
+        type: String,
+        minlength: 3,
+        maxlegth: 20,
+        trim: true,
+    },
+    emailID: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    age: {
+        type: Number,
+        min: 5,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    problemSolved: {
+        type: [String],
+    }
+
+}, { timestemps: ture })
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
