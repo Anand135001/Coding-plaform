bfe741cc682f37b726d05b574cfc418b9ad0b978
Anand <aanandmainwal@gmail.com>
2025-08-10 06:36:27 +0530

    Implement login feature and fixing user resgister issue

--- backend/src/controllers/userAuthController.js
+++ backend/src/controllers/userAuthController.js
@@ -13,17 +13,58 @@ const register = async(req, res) => {
         }
         
         const { firstname, emailID, password} = req.body;
-        req.body.password = bcrypt.hash(password, 10);
+        req.body.password = await bcrypt.hash(password, 10);
 
         const user = await User.create(req.body);
         // ==== Token =====
         const token = jwt.sign({_id:user._id, emailID: emailID}, process.env.JWT_KEY, {expiresIn: 60*60});
-        res.cookie('jwt', token, {maxAge: 60*60*1000});
+        res.cookie('jwt', token, {
+            maxAge: 60*60*1000,
+            httpOnly: true,
+            secure: process.env.NODE_ENV,
+        });
         res.status(201).send("User register Successfully");
         
     }
 
     catch(err){
-        res.status(400).send("Error: "+err);
+        res.status(400).json({ error: "Registration failed" }); 
+        console.error(err); 
     }
 }
+
+
+const login = async (req, res) => {
+    
+    try{
+        const {emailID, password} = req.body;
+
+        if (!emailID || !password) {
+            return res.status(400).json({ error: "Email and password are required" });
+        }
+
+        const user = await User.findOne({emailID});
+        if(!user){
+            return res.status(401).json({error: "invaild credentials"})
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+        if(!match){
+            throw new Error("invaild Credentials"); 
+        }
+
+        const token = jwt.sign({_id:user._id, emailID: emailID}, process.env.JWT_KEY, {expiresIn: 60*60});
+        
+        res.cookie("jwt", token, {
+          maxAge: 60 * 60 * 1000,
+          httpOnly: true,
+          secure: process.env.NODE_ENV,
+        });
+        res.status(201).send("User register Successfully");
+    }
+    catch(err){
+        res.status(401).send("Error:" +err);
+    }
+}
+
+
