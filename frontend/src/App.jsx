10583243a53f882568146ee1e4e1b6876ede3e1d
Anand <aanandmainwal@gmail.com>
2025-10-23 12:45:34 +0530

    building signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router";
+import Homepage from "./pages/homepage";
+import SignUp from "./pages/signup";
+import Login from "./pages/Login";
 
 function App(){
    
   return (
     <> 
       <Routes>
-         <Route path="/" element={<Homepage />} />
-         <Route path="/login" element={<Login />} />
-         <Route path="/signup" element={<Signup />} />
+         <Route path="/" element={<Homepage></Homepage>} />
+         <Route path="/login" element={<Login></Login>} />
+         <Route path="/signup" element={<SignUp></SignUp>} />
       </Routes>
     </>
   )
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,12 +1,51 @@
-
+// import { useState } from "react";
+import { useForm } from 'react-hook-form';
 
 function SignUp(){
+    
+  const { register, handleSubmit } = useForm();
 
-    return (
-        <>
-          SignUp page
-        </>
-    )
+  const submitData = (data) =>{ 
+    console.log(data)
+  };
+
+  return (
+    <>
+    <form onSubmit={handleSubmit(submitData)}>
+      <input {...register('firstName')}
+      placeholder='Enter firstName'/>
+      <input {...register('email')}
+      placeholder='Enter email'/>
+      <input {...register('password')}
+      placeholder='enter password'/>
+      <button type='submit' className='btn btn-sm'>Submit</button>
+    </form>
+    </>
+  )
+   
 }
 
 export default SignUp;
+
+
+
+//  const [name, setName] = useState('');
+//     const [email, setEmail ]= useState('');
+//     const [password, setPassword] = useState('');
+
+    
+//     const handleSubmit = (e) => {
+      
+//       e.preventDefault();
+//       console.log(name, email, password)
+      
+//     }
+
+//     return (
+//         <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-y-3"> 
+//           <input type="text" value={name} placeholder="Enter your firstName" onChange={(e) => setName(e.target.value)}></input>
+//           <input type="email" value={email} placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)}></input>
+//           <input type="password" value={password} placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}></input>
+//           <button type="submit">Submit</button>
+//         </form>
+//     )
