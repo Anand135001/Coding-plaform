70ebf27e3b23dd051024209fc8b65dc8803f58d5
Anand <aanandmainwal@gmail.com>
2025-10-27 23:52:40 +0530

    Add checkAuth route to check if present token in browser is vaild or not

--- backend/src/routes/userAuthRoute.js
+++ backend/src/routes/userAuthRoute.js
@@ -2,13 +2,14 @@ const express = require('express');
 const userMiddleware = require('../middleware/userAuthMiddleware');
 const adminMiddleware = require("../middleware/adminMiddleware");
 const authRouter = express.Router();
-const {register, login, logout, adminRegister, deleteProfile} = require('../controllers/userAuthController')
+const {register, login, logout, adminRegister, deleteProfile, checkAuth} = require('../controllers/userAuthController')
 
 authRouter.post("/register", register);
 authRouter.post('/login', login);
 authRouter.post("/logout", userMiddleware, logout);
 authRouter.post('/admin/register',adminMiddleware, adminRegister);
 authRouter.delete('profile',  userMiddleware, deleteProfile);
+authRouter.get("/check", userMiddleware, checkAuth);
 // authRouter.get("/getprofile", getprofile);
 
 
