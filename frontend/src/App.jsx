802c2f3c3dc1f712a56ee0717f77d2cb8ef4cd65
Anand <aanandmainwal@gmail.com>
2025-11-02 12:27:15 +0530

    Implement user authentication routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Homepage from "./pages/homepage";
 import SignUp from "./pages/signup";
 import Login from "./pages/Login";
+import { checkAuth } from "./utils/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App(){
-   
+
+  // route user on the bases user auth 
+  const { isAuthenticated } = useSelector((state)=> state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
   return (
     <> 
       <Routes>
-         <Route path="/" element={<Homepage></Homepage>} />
-         <Route path="/login" element={<Login></Login>} />
-         <Route path="/signup" element={<SignUp></SignUp>} />
+         <Route path="/" element={ isAuthenticated ? <Homepage />: <Navigate to="/login"/> } />
+         <Route path="/login" element={ isAuthenticated ? <Navigate to="/" /> : <Login />} />
+         <Route path="/signup" element={ isAuthenticated ? <Navigate to="/" />: <SignUp />} />
       </Routes>
     </>
   )
