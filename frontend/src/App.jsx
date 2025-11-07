320ff4afbce798692e4c5b42b962345013485d47
Anand <aanandmainwal@gmail.com>
2025-11-07 23:37:26 +0530

    Fix import case for SignUp component and enhance loading state handling in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router";
 import Homepage from "./pages/homepage";
-import SignUp from "./pages/signup";
+import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import { checkAuth } from "./utils/authSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,19 +9,27 @@ import { useEffect } from "react";
 function App(){
 
   // route user on the bases user auth 
-  const { isAuthenticated } = useSelector((state)=> state.auth);
+  const { isAuthenticated, loading } = useSelector((state)=> state.auth);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(checkAuth());
+      dispatch(checkAuth());
   }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+      </div>
+    );
+  }
 
   return (
     <> 
       <Routes>
-         <Route path="/" element={ isAuthenticated ? <Homepage />: <Navigate to="/login"/> } />
+         <Route path="/" element={ isAuthenticated ? <Homepage />: <Navigate to="/signup"/> } />
          <Route path="/login" element={ isAuthenticated ? <Navigate to="/" /> : <Login />} />
-         <Route path="/signup" element={ isAuthenticated ? <Navigate to="/" />: <SignUp />} />
+         <Route path="/signup" element={ isAuthenticated ? <Navigate to="/" /> : <SignUp />} />
       </Routes>
     </>
   )
