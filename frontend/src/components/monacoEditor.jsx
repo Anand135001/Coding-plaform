fef6a2ce78b00663828368758d65271e64b74e54
Anand <aanandmainwal@gmail.com>
2025-11-19 18:06:24 +0530

    testing monaco editor

---
+++ frontend/src/components/monacoEditor.jsx
@@ -0,0 +1,7 @@
+import Editor from '@monaco-editor/react';
+
+function MonacoEditor() {
+  return <Editor height="90vh" defaultLanguage="javascript" defaultValue="// some comment" theme='vs-dark' />;
+}
+
+export default MonacoEditor;
