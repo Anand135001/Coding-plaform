0d997bdf49c80d5a12de982f741f30ce055a734d
Anand <aanandmainwal@gmail.com>
2025-11-23 21:50:51 +0530

    Fix import case sensitivity and enhance user role checks in routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
 import { Routes, Route, Navigate } from "react-router";
-import Homepage from "./pages/homepage";
+import Homepage from "./pages/Homepage";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import { checkAuth } from "./utils/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import MonacoEditor from "./components/monacoEditor";
+import ProblemPage from './pages/Problempage';
+import Admin from './pages/Admin'
+import AdminPanel from './components/AdminPanel'
 
 function App(){
 
-  // route user on the bases user auth 
-  const { isAuthenticated, loading } = useSelector((state)=> state.auth);
+  const { isAuthenticated, loading, user} = useSelector((state)=> state.auth);
   const dispatch = useDispatch();
 
   useEffect(() => {
-      dispatch(checkAuth());
+    dispatch(checkAuth());
   }, [dispatch]);
 
   if (loading) {
@@ -28,8 +31,13 @@ function App(){
     <> 
       <Routes>
          <Route path="/" element={ isAuthenticated ? <Homepage />: <Navigate to="/signup"/> } />
-         <Route path="/login" element={ isAuthenticated ? <Navigate to="/" /> : <Login />} />
-         <Route path="/signup" element={ isAuthenticated ? <Navigate to="/" /> : <SignUp />} />
+         <Route path="/login" element={ isAuthenticated ? <Navigate to="/" /> : <Login /> } />
+         <Route path="/signup" element={ isAuthenticated ? <Navigate to="/" /> : <SignUp /> } />
+
+         <Route path="/admin" element={isAuthenticated && user?.role === 'admin' ? <Admin /> : <Navigate to="/" />} />
+         <Route path="/admin/create" element={isAuthenticated && user?.role === 'admin' ? <AdminPanel /> : <Navigate to="/" />} />
+         <Route path="/problem/:problemId" element={<ProblemPage/>}></Route>
+         
       </Routes>
     </>
   )
