8c48f59c6c201f82b4a52d17c8659d8add419fd7
Anand <aanandmainwal@gmail.com>
2025-11-26 16:13:12 +0530

    Rename route for fetching problem by ID to improve clarity

--- backend/src/routes/problemCreatorRoute.js
+++ backend/src/routes/problemCreatorRoute.js
@@ -10,7 +10,7 @@ problemRouter.post("/create",adminMiddleware, createProblem);
 problemRouter.put("/update/:id", adminMiddleware, updateProblem);
 problemRouter.delete("/delete/:id", adminMiddleware, deleteProblem);
 
-problemRouter.get("/getProblem/:id", userMiddleware,getProblem);
+problemRouter.get("/problemById/:id", userMiddleware, getProblem);
 problemRouter.get("/getAllProblem", userMiddleware, getAllProblem);
 problemRouter.get("/problemSolvedByUser", userMiddleware, solvedAllProblemByUser);
 problemRouter.get('/submittedProblem/:pid', userMiddleware, submittedProblem);
