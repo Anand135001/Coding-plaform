5a96194b504a720cf646509d9f7ef2751f1d59e0
Anand <aanandmainwal@gmail.com>
2026-03-17 14:52:40 +0530

    fix: update navigation link text from 'LeetCode' to 'RootCode' in Homepage component

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -49,7 +49,7 @@ function Homepage() {
       {/* Navigation Bar */}
       <nav className="navbar bg-base-100 shadow-lg px-4">
         <div className="flex-1">
-          <NavLink to="/" className="btn btn-ghost text-xl">LeetCode</NavLink>
+          <NavLink to="/" className="btn btn-ghost text-xl">RootCode</NavLink>
         </div>
         <div className="flex-none gap-4">
           {user && (<NavLink to="/profile" className="btn btn-ghost" > {user.firstname} </NavLink>)}
